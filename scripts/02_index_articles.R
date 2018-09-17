6d53ed628cbd39c64cf5a668aebd513e8c306fe4
Karissa <whiting@ecohealthalliance.org>
2018-09-17 17:46:25 -0400

    standardizing naming of files to better track data errors. resolved missing article data problem and updated index files

--- scripts/02_index_articles.R
+++ scripts/02_index_articles.R
@@ -24,16 +24,6 @@ art <- map(files, ~read_csv_art(fileloc, .x))%>%
     x }) %>%
   rename("author_year" = x1)
 
-# files <- dir(path = here('data-raw', 'art_index_csvs'), pattern = "*.csv", full.names = TRUE)
-# art <- map(files, ~read_csv(.x, col_types = cols("STUDY_ID" = col_integer(),
-#                                                  "year" = col_integer(), 
-#                                                  "pmid" = col_integer(),
-#                                                  .default = "c"), local = locale(encoding = "latin1"))) %>%
-#   map_df(function(x) {
-#     names(x) %<>% tolower
-#     x }) %>%
-#   rename("author_year" = x1)
-
 # check if there are any missing study ID's from original RData assignent dataframe
 
 original_art <- readRDS(here("data", "original_full_text.rds"))
@@ -50,16 +40,18 @@ art %>% group_by(downloaded) %>%
 
 #clean downloaded column, and write to master index csv
 art <- art %>%
-  mutate(reason = ifelse(art$downloaded %in% c("not full text", "could not access"),
-                          art$downloaded, art$reason), 
+  mutate(reason = ifelse(.$downloaded %in% c("not full text", "could not access"),
+                          .$downloaded, .$reason), 
          downloaded = fct_collapse(downloaded, 
                                      yes = c("yes", "downloaded"), 
-                                     no = c("no", "not full text", "could not access")))
+                                     no = c("no", "not full text", "could not access"))) %>%
+  mutate(in_codes_db = ifelse(.$downloaded == "yes", "yes", "no"))
 
 write.csv(art, file = here("data", "index_articles.csv"))
 save(art, file = here("data", "index_articles.RData"))
 
 # count number of articles that were able to be downloaded
 fct_count(art$downloaded)
+fct_count(art$in_codes_db)
 fct_count(art$include)
 
--- scripts/compare_seg_to_art_index.R
+++ scripts/compare_seg_to_art_index.R
@@ -5,28 +5,44 @@ library(magrittr)
 
 load(here("data", "index_articles.RData"))
 load(file = here("data", "coded_annotations.RData"))
+load(file = here("data", "segments.RData"))
 
 
 art_ids <- art %>%
-  filter(downloaded == "yes") %>%
+  filter(in_codes_db == "yes") %>%
   pull(study_id) %>%
   unique
 
-segments_id <-  codes %>%
+segments_id <-  segments %>%
   pull(study_id) %>%
   unique
 
-diff1 <- setdiff(art_ids, segments_id)
-# 68 - need to check if these are zeros
+in_art_not_codes <- setdiff(art_ids, segments_id)
+# 26 of these (exported to check_missing_in_codes.csv) -- looked into them and recoded updates in check_missing_in_code_edit.csv 
 
-diff2 <- setdiff(segments_id, art_ids)
-# 13233 is typo on art_index, should be 13223 I think
-# 14751 is marked as not downloaded but appears in segments, so it was downloaded
-# 20344 - I think this might be 20334 and a typo in study id in the article index
-# 20781 marked as no downloaded but it was downloaded and marked with an exclusion tag
+in_codes_not_art <- setdiff(segments_id, art_ids)
+# 4 of these --all fixed (See below)
+
+# 13233 is typo on art_index, should be 13223 I think -- fixed
+# 14751 is marked as not downloaded but appears in segments, so it was downloaded -- fixed
+# 20344 - I think this might be 20334 and a typo in study id in the article index -- fixed
+# 20781 marked as no downloaded but it was downloaded and marked with an exclusion tag -- fixed
 
 missing_in_codes <- 
-  art %>% filter(., study_id %in% diff1)
-#some of these have notes that they could not be highlighted in maxqda. let's add a column in article index db separate from downloaded that indicated whether it is included in the db or not
-write.csv(missing_in_codes, file = here("data", "check_missing_in_codes.csv"))
-     
+  art %>% filter(., study_id %in% in_art_not_codes)
+
+missing_in_codes <- missing_in_codes %>%
+  mutate(in_codes_db = ifelse(!is.na(reason), "no", "check_mex_files"))
+
+# some of these have notes that they could not be highlighted in maxqda. let's add a column in article index db separate from downloaded that indicated whether it is included in the db or not
+write_csv(missing_in_codes, file = here("data", "check_missing_in_codes.csv"))
+
+# manually check missing ones
+fix_missing <- read_csv(here("data", "check_missing_in_codes_edit.csv"))
+
+art <- art  %>%
+  filter(!(study_id %in% fix_missing$study_id)) %>%
+  rbind(fix_missing)
+
+
+  
