cb447fe48404b197623e1a929f367be579f2e86b
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2018-10-18 17:03:22 -0400

    restructure mesh and drug name cleaning

---
+++ scripts/05_clean_drugs.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(magrittr)
+library(stringi)
+library(here)
+library(googlesheets)
+
+# Structure Drugs Data and Manual Corrections-----------------
+
+segments_db <- read_rds(here("data", "segments_db.rds")) 
+cleaned_drug_codes <- gs_read(gs_title("amr_db_clean_drugs")) %>% as.tibble() %>% filter(new!="--") %>% #google spreadsheet with field cleanup
+  mutate(segment = paste0("^", segment, "$"))
+
+# structure segments database into drug codes dataframe   
+drugs <- segments_db %>%
+  filter(code_main == "drug resisted") %>%
+  select(-code_main_cat) %>%
+  mutate(segment = stri_replace_all_regex(segment, 
+                                          c("\\(|\\)|\\:|\\;|\006|\002|\\.|\\,", "\\+|\\–"),
+                                          c("", "/"), vectorize = FALSE)) %>%         
+  mutate(segment = str_trim(segment)) %>%
+  mutate(segment = stri_replace_all_regex(segment,  cleaned_drug_codes$segment, cleaned_drug_codes$new, vectorize_all = FALSE)) 
+
+# separate drug combos
+drugs %<>%
+  mutate(segment_combo = ifelse(grepl("\\/", segment), TRUE, FALSE)) %>%
+  mutate(segment = str_split(segment, "\\/")) %>%
+  unnest()
+
+# identify duplicate drugs in studies (to be checked)
+dups_with_same_code <- drugs %>%
+  group_by(study_id, segment_combo, code_identifiers, segment) %>%
+  filter(duplicated(segment) | duplicated(segment, fromLast = TRUE)) 
+
+dups_with_NA_code <- drugs %>%  
+  group_by(study_id, segment_combo, segment) %>%
+  filter(duplicated(segment) | duplicated(segment, fromLast = TRUE)) %>%
+  mutate(code_identifiers = ifelse(code_identifiers=="", "NA", code_identifiers)) %>%
+  summarize(code_identifiers = paste(code_identifiers, collapse="|")) %>%
+  filter(grepl("NA", code_identifiers))
+
+# Join segments with MESH drug ontology-----------------
+mesh0 <- read_rds(here("data","mesh-ontology", "mesh_raw.rds")) #raw mesh data from clean_mesh.r
+mesh <- read_rds(here("data","mesh-ontology", "mesh.rds")) #mesh data from clean_mesh.r
+
+drugs %<>% left_join(., mesh) 
+
+# preferred name look ups for synonyms
+drugs %<>%
+  left_join(., mesh0 %>% select(class_id, preferred_label), by = c("mesh_id"="class_id")) %>%
+  rename(mesh_preferred_label = preferred_label) 
+
+# get mesh class (c or d) and associated parent ID
+# c = Class 1 Supplementary Records - Chemicals (These records are dedicated to chemicals and are primarily heading mapped to the D tree descriptors.)
+# d = D tree for drugs and chemicals,
+drugs %<>%
+  mutate(mesh_class = substr(mesh_id, 1, 1)) %>%
+  mutate(mesh_class_desc = ifelse(mesh_class=="c", "supp", "desc")) %>%
+  mutate(mesh_parent_id = ifelse(mesh_class=="c", mesh_hm, mesh_parents)) %>%
+  separate(mesh_parent_id, c("mesh_parent_id", "mesh_parent_id_qualifier"), "/") %>% #warning is ok
+  mutate(mesh_parent_id = str_split(mesh_parent_id, "\\|")) %>%  unnest() %>%
+  mutate(mesh_pa_id = str_split(mesh_pa, "\\|")) %>%  unnest()
+
+# get mesh parent name and pharmacological action (pa) name
+drugs %<>%
+  left_join(., mesh0 %>% select(class_id, preferred_label ), by = c("mesh_parent_id"= "class_id")) %>%
+  rename("mesh_parent_name" = preferred_label) %>%
+  left_join(., mesh0 %>% select(class_id, preferred_label ), by = c("mesh_pa_id"= "class_id")) %>%
+  rename("mesh_pa_name" = preferred_label) %>%
+  select(-mesh_hm, -mesh_parents, -mesh_pa)
+#TODO get qualifier name
+
+# check matches
+drugs_unique <- drugs %>%
+  select(-study_id, -code_identifiers, -code_main) %>%
+  unique()
+
+missing <- drugs_unique %>%
+  filter(is.na(mesh_id))  
+not_missing <- drugs_unique %>%
+  filter(!is.na(mesh_id))  
---
+++ scripts/clean_mesh.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(magrittr)
+library(here)
+library(janitor)
+
+###MeSH Medical Subject Headings (https://meshb.nlm.nih.gov/search)###
+
+# Read raw mesh db and cleam
+mesh0 <- read_csv(here("data", "mesh-ontology", "as_received", "MESH.csv")) %>% #downloaded from bioportal: https://bioportal.bioontology.org/ontologies/MESH?p=summary
+  clean_names() %>%
+  select(class_id, preferred_label, synonyms, definitions, hm, parents, pa, mn) %>%
+  mutate_at(vars(class_id, parents), funs(gsub("http://purl.bioontology.org/ontology/MESH/", "", .))) %>%
+  mutate_all(tolower) 
+
+# Separate out synonyms, reshape, rename
+mesh <- mesh0 %>%
+  mutate(synonyms = str_split(synonyms, "\\|")) %>% unnest() %>%
+  gather(key = "segment_name_class", value = "segment", preferred_label, synonyms, 
+         -class_id, -definitions, -hm, -parents, -pa) %>% unique() %>%
+  filter(!is.na(segment)) %>% #cases with no synonyms
+  rename_all(funs(paste0('mesh_', .)))  %>%
+  rename(segment = mesh_segment, mesh_id = mesh_class_id)
+
+# filter out synonyms that are also preferred labels
+pref_names <- mesh %>%
+  filter(mesh_segment_name_class == "preferred_label") %>%
+  pull(segment) %>%
+  unique()
+
+mesh %<>% filter(!c(segment %in% pref_names & mesh_segment_name_class == "synonyms"))
+
+# save data
+write_rds(mesh0, here("data","mesh-ontology", "mesh_raw.rds"))
+write_rds(mesh, here("data","mesh-ontology", "mesh.rds"))
+
+#c = Class 1 Supplementary Records - Chemicals #These records are dedicated to chemicals and are primarily heading mapped to the D tree descriptors.
+#d = D tree for drugs and chemicals,
+#use MN to get full tree - only works for D.  C lookup against D (HM) then full tree
+#test <- mesh0[22,]
+#test2 <- filter(mesh, mn=="C01.252.400.780.790")
