d17336b6f457d60430632cb2816f87bd858d8df3
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2018-10-22 14:28:57 -0400

    tweaks to drug script, bacteria script update to directly access NCBI for bacteria genus/species and CARD for strains and resistance markers, cleaning of bacteria names.

---
+++ scripts/06_clean_bacteria.R
@@ -0,0 +1,186 @@
+library(tidyverse)
+library(magrittr)
+library(stringi)
+library(here)
+library(googlesheets)
+
+#checks:
+#dups (NA)
+#clean (amr_db_clean_bacteria - issue - includes MAXQDA edits)
+#missing (amr_db_missing_bacteria - issue #10)
+
+# Structure Bacteria Data-----------------
+
+segments_db <- read_csv(here("data", "segments_db.csv"))
+cleaned_bacteria_codes <-
+  gs_read(gs_title("amr_db_clean_bacteria")) %>% as.tibble() %>% filter(new != "--") %>% #google spreadsheet with field cleanup
+  mutate(segment = paste0("^", segment, "$"))
+
+# structure segments database into bacteria codes dataframe with initial cleaning
+bacteria <- segments_db %>%
+  filter(code_main_cat == "bacteria")  %>%
+  select(-code_main_cat) %>%
+  mutate(segment = stri_replace_all_regex(segment,
+                                          c("\\(|\\)|\\:|\\;|\\.|\\,"),
+                                          c(""), vectorize = FALSE)) %>%
+  mutate(
+    segment = stri_replace_all_regex(
+      segment,
+      cleaned_bacteria_codes$segment,
+      cleaned_bacteria_codes$new,
+      vectorize_all = FALSE
+    )
+  )
+
+# id studies with missing bacteria segments
+bacteria_genspe <-
+  bacteria %>% filter(code_main == "binomial (genus species)") %>% pull(study_id)
+missing_genspe <-
+  segments_db %>% filter(!study_id %in% bacteria_genspe) %>% pull(study_id) %>% unique() #45 missing
+
+bacteria_marker <-
+  bacteria %>% filter(code_main == "resistance marker") %>% pull(study_id)
+missing_marker <-
+  segments_db %>% filter(!study_id %in% bacteria_marker) %>% pull(study_id) %>% unique()
+
+bacteria_strain <-
+  bacteria %>% filter(code_main == "strain") %>% pull(study_id)
+missing_strain <-
+  segments_db %>% filter(!study_id %in% bacteria_strain) %>% pull(study_id) %>% unique()
+
+#articles_db <- read_rds(here("data", "articles_db.rds")) %>%
+#  select(study_id, mex_name) %>%
+#  filter(study_id %in% missing_genspe) #push to google drive
+#gs_new("amr_db_missing_bacteria", input=articles_db)
+
+# identify duplicate bacteria in studies (to be checked)
+dups_with_same_code <- bacteria %>%
+  group_by(study_id, code_identifiers, code_main, segment) %>%
+  filter(duplicated(segment) |
+           duplicated(segment, fromLast = TRUE))
+
+dups_with_NA_code <-  bacteria %>%
+  group_by(study_id, code_main, segment) %>%
+  filter(duplicated(segment) |
+           duplicated(segment, fromLast = TRUE)) %>%
+  mutate(code_identifiers = ifelse(code_identifiers == "", "NA", code_identifiers)) %>%
+  summarize(code_identifiers = paste(code_identifiers, collapse = "|")) %>%
+  filter(grepl("NA", code_identifiers))
+
+# NCBI Ontology-----------------
+# for bacteria genus / species
+
+ncbi0 <- read_rds(here("data", "ncbi-ontology", "ncbi_raw.rds")) #raw ncbi data from clean_ncbi.r
+ncbi <- read_rds(here("data", "ncbi-ontology", "ncbi.rds")) #ncbi data from clean_ncbi.r
+
+# get abbreviated gen/spe
+ncbi_abbr <- ncbi %>%
+  filter(ncbi_div == "bacteria", ncbi_rank == "species") %>%
+  mutate(segment = paste(substr(segment, 1, 1), gsub(".* ", "", segment)),
+         ncbi_segment_name_class = "abbreviation")
+
+ncbi %<>% bind_rows(., ncbi_abbr)
+
+# join into bacteria data frame
+bacteria %<>% left_join(., ncbi)
+
+# preferred name look ups for synonyms and abbreviations
+bacteria %<>%
+  left_join(.,
+            ncbi0 %>% select(class_id, preferred_label),
+            by = c("ncbi_id" = "class_id")) %>%
+  rename(ncbi_preferred_label = preferred_label)
+
+# get ncbi parent name
+bacteria %<>%
+  left_join(.,
+            ncbi0 %>% select(class_id, preferred_label),
+            by = c("ncbi_parents" = "class_id")) %>%
+  rename("ncbi_parent_name" = preferred_label) %>%
+  select(-ncbi_parents)
+
+# check matches
+bacteria_unique <- bacteria %>%
+  select(segment, ncbi_id, code_main) %>%
+  unique()
+
+no_match <- bacteria_unique %>%
+  filter(is.na(ncbi_id))  %>%
+  group_by(code_main) %>%
+  count()
+
+match <- bacteria_unique %>%
+  filter(!is.na(ncbi_id))  %>%
+  group_by(code_main) %>%
+  count()
+
+# CARD Ontology-----------------
+# for strains and markers
+
+# load card + functions
+source(here("scripts", "clean_card.R"))
+
+# get card id and parents + ancestors
+bacteria %<>%
+  mutate(card_db = ifelse(code_main == "binomial (genus species)", "card_ncbi", "card_aro")) %>%
+  mutate(
+    card_select_id = map_chr(map2(
+      .x = segment, .y = card_db, get_select_id
+    ), "select_id"),
+    card_select_segment_class = map_chr(
+      map2(.x = segment, .y = card_db, get_select_id),
+      "select_segment_class"
+    ),
+    card_parent_name = map(
+      map2(.x = card_select_id, .y = card_db, get_relative, "parents"),
+      "relative_name"
+    ),
+    card_parent_id = map(
+      map2(.x = card_select_id, .y = card_db, get_relative, "parents"),
+      "relative_id"
+    )
+  ) %>%
+  unnest() %>%
+  mutate(
+    card_ancestor_name = map(
+      map2(.x = card_select_id, .y = card_db, get_relative, "ancestors"),
+      "relative_name"
+    ),
+    card_ancestor_id = map(
+      map2(.x = card_select_id, .y = card_db, get_relative, "ancestors"),
+      "relative_id"
+    )
+  ) %>%
+  unnest() %>%
+  filter(card_ancestor_name != segment) %>%
+  group_by(study_id, code_identifiers, card_select_id) %>%
+  mutate(card_ancestor_rank = 1:n()) %>%
+  ungroup()
+
+# check matches
+bacteria_unique <- bacteria %>%
+  select(segment, card_db, card_select_id, code_main) %>%
+  unique()
+
+no_match <- bacteria_unique %>%
+  filter(card_select_id == "no exact match")  %>%
+  group_by(code_main) %>%
+  count()
+
+match <- bacteria_unique %>%
+  filter(card_select_id != "no exact match")  %>%
+  group_by(code_main) %>%
+  count()
+
+# Separate DBs-----------------
+bacteria_ncbi <- bacteria %>%
+  filter(code_main == "binomial (genus species)") %>%
+  select("study_id", "segment", "code_main", "code_identifiers", starts_with("ncbi"))
+
+write_csv(bacteria_ncbi, here("data", "bacteria_genus_species_db.csv"))
+
+bacteria_card <- bacteria %>%
+  filter(code_main != "binomial (genus species)") %>%
+  select("study_id", "segment", "code_main", "code_identifiers", starts_with("card"))
+
+write_csv(bacteria_card, here("data", "bacteria_strains_and_resistance_markers_db.csv"))
