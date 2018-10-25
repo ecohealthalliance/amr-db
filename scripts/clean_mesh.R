c4bf647615902c950b18d0f4cb99f14f1fa42c5a
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2018-10-25 14:59:17 -0400

    edit field names for clarity

--- scripts/clean_mesh.R
+++ scripts/clean_mesh.R
@@ -5,7 +5,7 @@ library(janitor)
 
 ###MeSH Medical Subject Headings (https://meshb.nlm.nih.gov/search)###
 
-# Read raw mesh db and cleam
+# Read raw mesh db and clean
 mesh0 <- read_csv(here("data-raw", "mesh-ontology", "as_received", "MESH.csv.zip")) %>% #downloaded directly from bioportal: https://bioportal.bioontology.org/ontologies/MESH?p=summary
   clean_names() %>%
   select(class_id, preferred_label, synonyms, definitions, hm, parents, pa, mn) %>%
@@ -18,16 +18,16 @@ mesh <- mesh0 %>%
   gather(key = "segment_name_class", value = "segment", preferred_label, synonyms, 
          -class_id, -definitions, -hm, -parents, -pa) %>% unique() %>%
   filter(!is.na(segment)) %>% #cases with no synonyms
-  rename_all(funs(paste0('mesh_', .)))  %>%
-  rename(segment = mesh_segment, mesh_id = mesh_class_id)
+  rename_all(funs(paste0('drug_', .)))  %>%
+  rename(segment = drug_segment, drug_id = drug_class_id)
 
 # filter out synonyms that are also preferred labels
 pref_names <- mesh %>%
-  filter(mesh_segment_name_class == "preferred_label") %>%
+  filter(drug_segment_name_class == "preferred_label") %>%
   pull(segment) %>%
   unique()
 
-mesh %<>% filter(!c(segment %in% pref_names & mesh_segment_name_class == "synonyms"))
+mesh %<>% filter(!c(segment %in% pref_names & drug_segment_name_class == "synonyms"))
 
 # save data
 write_rds(mesh, here("data-raw","mesh-ontology", "mesh.rds"))
