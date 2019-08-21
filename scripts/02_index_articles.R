87382872d3e3c4290901bdc1c50653048358a848
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-08-21 16:45:14 -0400

    More fixes to mex files and update QA checks in index_articles

--- scripts/02_index_articles.R
+++ scripts/02_index_articles.R
@@ -5,18 +5,19 @@ library(magrittr)
 library(compare)
 
 # Clean Article Index Data -------------------
+# and make sure all articles are accounted for in csvs, mex files, and segment exports
 
-# read in all article index files from individual reviewers
+# Read in all article index files from individual reviewers
 read_article_csvs <- function(fileloc, filename){
   each_article_csv <- read_csv(paste(fileloc, filename, sep = "/"), 
                                col_types = cols("STUDY_ID" = col_integer(),
                                                 .default = "c"), 
                                local = locale(encoding = "latin1")) %>%
     filter(!is.na(STUDY_ID))  %>% #remove rows that are all NA
-    mutate(csv_name = filename)
+    mutate(csv_name = filename) %>%
+    mutate(DOWNLOADED = tolower(DOWNLOADED))
   return(each_article_csv)
 }
-
 
 fileloc <- here("data-raw", "art_index_csvs")
 files <- list.files(fileloc)
@@ -25,7 +26,17 @@ articles_db <- map_df(files, ~read_article_csvs(fileloc, .x)) %>%
   rename_all(tolower) %>%
   rename("author_year" = x1)
 
-# read in mex files to link document names
+# Cleaning csv index files - the dupes are studies that were originally marked as downloaded but were not (identified in QA below) - they were coded by SD or EM
+dupes_csv <- janitor::get_dupes(articles_db, study_id) %>%
+  group_by(study_id) %>%
+  summarize(new_reviewer = any(str_detect(unique(csv_name), "sd|em"))) %>%
+  ungroup()
+all(dupes_csv$new_reviewer)
+dupes_csv_study_id <- dupes_csv %>% pull(study_id)
+articles_db %<>%
+  filter(!(study_id %in% dupes_csv_study_id & !str_detect(csv_name, "sd|em")))
+
+# Read in mex files to link document names
 src_sqlite_mex <- function(fileloc, filename){
   path <- src_sqlite(paste(fileloc, filename, sep = "/"))
   table <- tbl(path, "Texts") %>% 
@@ -40,19 +51,43 @@ src_sqlite_mex <- function(fileloc, filename){
 
 fileloc <- here("data-raw", "coded_text_mex")
 files <- list.files("data-raw/coded_text_mex/", pattern = "*.mex")
-mex <- map_df(files, ~src_sqlite_mex(fileloc, .x))
+mex <- map_df(files, ~src_sqlite_mex(fileloc, .x)) 
 
-## QA checks
-# study ids in csvs but not mex files 
+# Check for mex file dupes - NEED TO ADDRESS
+dupes_mex <- janitor::get_dupes(mex, study_id)
+
+# Any study ids in csvs but not mex files?
 articles_db %>%
   filter(!study_id %in% mex$study_id) %>%
-  filter(downloaded %in% c("YES", "yes", "Downloaded", NA_character_)) # these were supposedly downloaded - why not in mex files?
+  filter(downloaded %in% c("yes", "downloaded", NA_character_)) 
   # Those that could NOT be downloaded are addressed in issue #4
 
-# this study is in mex but not indexed
+# Any study ids in mex but not csv?
 mex$study_id[!mex$study_id %in% articles_db$study_id]
 
+# Join mex file names into index
 articles_db <- left_join(articles_db, mex)
+
+# Now, make sure exports (segments) match index
+files <- dir(path = here('data', 'coded_segments'), pattern = "*.xlsx", full.names = TRUE)
+segments_raw <- map_dfr(files, ~read_xlsx(.x, col_types = "text"))
+
+articles_db %>%
+  filter(downloaded %in% c("yes", "downloaded")) %>%
+  filter(!study_id %in% unique(segments_raw$`Document name`)) # articles in mex but not in segments
+
+segments_raw %>%
+  filter(!`Document name` %in% unique(articles_db$study_id)) # articles in segments but not in mex/csvs
+
+# check for dupes in segments (rough check- making sure any article with more than one author has a new reviewer- there shouldn't be an article coded by two old reviewers)
+segments_raw %>%
+  select(`Document name`, Author) %>%
+  distinct() %>%
+  janitor::get_dupes(., `Document name`) %>%
+  group_by(`Document name`) %>%
+  mutate(new_reviewer = any(str_detect(unique(Author), "Sonia|chen|dattaray"))) %>%
+  ungroup() %>%
+  filter(new_reviewer == FALSE)
 
 # clean up article index dataframe
 articles_db <- articles_db %>%
