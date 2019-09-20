32a3edd3742560dfc6b67e59b67f54ae6de97ec1
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-09-20 11:30:14 -0400

    New heatmaps

--- scripts/data_summary/data_summary.R
+++ scripts/data_summary/data_summary.R
@@ -1,11 +1,33 @@
 library(tidyverse)
 library(here)
+library(patchwork)
+
+# ggplot function ---------------------------------------------------------
+plot_heatmap <- function(dat){
+  
+  ggplot(dat,aes(bacteria, drug)) + 
+    scale_x_discrete(position = "top") +
+    geom_tile(color = "black", aes(fill = n), na.rm = FALSE, alpha = 0.5) +
+    geom_label(aes(label = n)) +
+    scale_fill_viridis_c(guide = FALSE, alpha = 0.5) +
+    labs(x = "", y="") +
+    theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) +
+    theme_minimal() +
+    theme(panel.grid.major = element_blank(),
+          axis.text = element_text(color = "black", size = 11))
+  
+}
+
+
+# Get data ----------------------------------------------------------------
 
 events <- read_csv(here("data", "events_db.csv")) %>%
   rename(drug = drug_preferred_label, bacteria = bacteria_preferred_label) %>%
   mutate(drug = str_remove_all(drug, "drug|combination")) %>%
   mutate(drug = str_replace_all(drug, ",", " + ")) %>%
-  mutate(drug = str_squish(drug))
+  mutate(drug = str_squish(drug)) %>%
+  mutate(drug = fct_infreq(drug)) %>%
+  mutate(bacteria = fct_infreq(bacteria))
 
 n_distinct(events$study_id)
 n_distinct(events$study_country)
@@ -17,11 +39,6 @@ events %>%
   group_by(study_country) %>%
   count(sort=T) 
 
-top_bact <- events %>%
-  group_by(bacteria) %>%
-  count(sort = TRUE) %>%
-  ungroup()
-
 top_drugs <- events %>% 
   select(bacteria, drug) %>%
   group_by(drug) %>%
@@ -32,21 +49,8 @@ top_drugs <- events %>%
   count(name = "n_bacteria") %>%
   ungroup() %>%
   arrange(-n_events) %>%
-  mutate(lab = ifelse(n_events == max(n_events), paste(n_bacteria, "resistant bacteria"), n_bacteria))
-
-top_drugs %>% 
-  slice(1:20) %>%
-  mutate(drug = fct_rev(fct_inorder(drug))) %>%
-  ggplot(., aes(x=drug, y=n_events)) +
-  geom_bar(stat = "identity", fill = "gray60" ) +
-  geom_text(aes(label=lab), y = 2, hjust = "left",  fontface = "bold") +
-  scale_x_discrete(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  labs(y = "Global AMR Resistance Events", x = "") +
-  coord_flip() +
-  theme_minimal() 
-ggsave(filename = here("scripts/data_summary/most-common-drugs.png"), width = 8, height = 6)
-
+  mutate(lab = ifelse(n_events == max(n_events), paste(n_bacteria, "unique resistant bacteria"), n_bacteria)) %>%
+  slice(1:12)
 
 top_bact <- events %>% 
   select(bacteria, drug) %>%
@@ -58,59 +62,69 @@ top_bact <- events %>%
   count(name = "n_drugs") %>%
   ungroup() %>%
   arrange(-n_events) %>%
-  mutate(lab = ifelse(n_events == max(n_events), paste(n_drugs, "drugs resisted"), n_drugs))
-
-top_bact %>% 
-  slice(1:20) %>%
-  mutate(bacteria = fct_rev(fct_inorder(bacteria))) %>%
-  ggplot(., aes(x=bacteria, y=n_events)) +
-  geom_bar(stat = "identity", fill = "gray60" ) +
-  geom_text(aes(label=lab), y = 2, hjust = "left",  fontface = "bold") +
-  scale_x_discrete(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  labs(y = "Global AMR Resistance Events", x = "") +
-  coord_flip() +
-  theme_minimal() 
-ggsave(filename = here("scripts/data_summary/most-common-bact.png"), width = 8, height = 6)
+  mutate(lab = ifelse(n_events == max(n_events), paste(n_drugs, "unique drugs resisted"), n_drugs)) %>%
+  slice(1:12)
 
 
-# Heatmap -----------------------------------------------------------------
-events %>%
+# Heatmaps -----------------------------------------------------------------
+
+heatmap_mat <- events %>%
   group_by(drug, bacteria) %>%
   count() %>%
   ungroup() %>%
-  filter(bacteria %in% top_bact$bacteria[top_bact$n_events>30]) %>%
-  filter(drug %in% top_drugs$drug[top_drugs$n_events>30]) %>%
   full_join(., expand(., drug, bacteria)) %>%
-  mutate(bacteria = factor(bacteria, levels = top_bact$bacteria, labels =  gsub(" ", "\n", top_bact$bacteria))) %>%
-  mutate(drug = factor(drug, levels = rev(top_drugs$drug))) %>%
-  ggplot(., aes(bacteria, drug)) + 
-  scale_x_discrete(position = "top") +
-  geom_tile(color = 'gray', aes(fill = n), na.rm = FALSE, alpha = 0.5) +
-  geom_label(aes(label = n)) +
+  filter(bacteria %in% top_bact$bacteria) %>%
+  filter(drug %in% top_drugs$drug) 
+
+heatmap <- ggplot(heatmap_mat %>% 
+                    mutate(bacteria = fct_relabel(bacteria, ~str_replace(., " ", "\n"))) %>%
+                    mutate(drug = fct_rev(drug)),
+                  aes(bacteria, drug)) + 
+  geom_tile(color = "black", aes(fill = n), alpha = 0.5 ) +
+  geom_text(aes(label = n)) +
+  scale_fill_viridis_c(guide = FALSE, alpha = 0.5, na.value="transparent") +
+  scale_x_discrete(position = "top", expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  labs(x = "", y = "") +
+  theme_bw() +
+  theme(axis.text.y = element_text(size = 12, angle = -25, vjust = 1),
+        axis.text.x = element_text(size = 10.5, face = "italic"),
+        axis.ticks = element_blank())
+
+drugs_total <- top_drugs %>% 
+  mutate(drug = fct_rev(drug)) %>%
+  ggplot(., aes(x = drug, y = n_events, fill = n_events)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=lab), y = 2, hjust = "left") +
   scale_fill_viridis_c(guide = FALSE, alpha = 0.5) +
-  labs(x = "", y="", size = "AMR Resistance Events") +
-  theme_minimal() +
-  theme(panel.grid.major = element_blank(),
-        axis.text = element_text(color = "black", size = 11))
-ggsave(filename = here("scripts/data_summary/drug-bacteria-heatmap.png"), width = 14, height = 7)
-
-# Publications over time --------------------------------------------------
-events %>%
-  mutate(start_year = as.numeric(str_sub(start_date, 1, 4))) %>%
-  group_by(start_year) %>%
-  count() %>%
-  ungroup() %>%
-  bind_rows(tibble(start_year = setdiff(seq(min(.$start_year), max(.$start_year)), .$start_year), n=0)) %>%
-  arrange(start_year) %>%
-  mutate(start_year = as.character(start_year)) %>%
-  ggplot(., aes(x=start_year, y=n)) +
-  geom_bar(stat = "identity", fill = "gray60" ) +
   scale_x_discrete(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 300)) +
-  labs(y = "Global AMR Resistance Events", x = "") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 60, vjust = 0.6))
+  scale_y_continuous(expand = c(0, 0), position = "right") +
+  labs(y = "", x = "") +
+  coord_flip() + 
+  theme_bw() +
+  theme(axis.text.y = element_blank(), 
+        axis.text.x = element_text(size = 14),
+        axis.title.x = element_text(size = 14, hjust = 1),
+        axis.ticks = element_blank())
 
-ggsave(filename = here("scripts/data_summary/pubs-by-year.png"), width = 8, height = 6)
+bact_total <- top_bact %>% 
+  ggplot(., aes(x = bacteria, y = n_events, fill = n_events)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=lab), y = -5, angle = 90, hjust = 1) +
+  scale_fill_viridis_c(guide = FALSE, alpha = 0.5) +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_reverse(expand = c(0, 0)) +
+  labs(y = "", x = "") +
+  theme_bw() +
+  theme(axis.text.x = element_blank(), 
+        axis.text.y = element_text(size = 14),
+        axis.title.y = element_text(size = 11, hjust = 0),
+        axis.ticks = element_blank())
+
+heatmap + drugs_total + bact_total + 
+  plot_layout(ncol = 2, widths = c(3, 1), heights = c(2, 1))
+
+
+ggsave(here("scripts/data_summary/drug-bacteria-combos.png"), width = 17, height = 9)
+
 
