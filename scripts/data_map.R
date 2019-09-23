e3d81af38abed6e13fe626010653553ba63e21db
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-09-23 13:11:31 -0400

    Figure and file structure modification; readme update

---
+++ scripts/data_map.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(here)
+library(RColorBrewer)
+library(sf)
+library(leaflet)
+library(leaflet.extras)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(countrycode)
+library(mapview)
+
+events <- read_csv(here("data", "events_db.csv"))
+
+locs <- read_csv(here("data", "locations.csv")) %>%
+  mutate(study_resolution = gsub(",.*$", "", study_location_basis)) %>%
+  filter(study_id %in% events$study_id)
+
+study_locs <- locs %>% 
+  filter(!is.na(study_location)) 
+  
+admin <- ne_countries(type='countries', scale = 'large') %>%
+  st_as_sf() %>%
+  mutate(iso3c = countrycode(sourcevar = name,
+                             origin = "country.name",
+                             destination = "iso3c"),
+         iso3c = ifelse(is.na(iso3c), iso_a3_eh, iso3c)) %>%
+  select(name, iso3c)
+
+events_sum <- events %>% 
+  mutate(iso3c = countrycode(sourcevar = study_country,
+                             origin = "country.name",
+                             destination = "iso3c")) %>%
+  group_by(iso3c) %>%
+  count(name = "AMR Events") %>%
+  ungroup() 
+  
+admin <- left_join(admin, events_sum, by = "iso3c") %>%
+  mutate(`AMR Events` = replace_na(`AMR Events`, 0))
+  
+pal <- colorNumeric("OrRd", domain = admin$`AMR Events`, na.color = "#e9e9f0")
+
+caption <- glue::glue(nrow(events), " AMR emergence events<br/>",
+                      n_distinct(events$study_country), " countries<br/>",
+                      n_distinct(events$drug_preferred_label), " antimicrobial drugs<br/>",
+                      n_distinct(events$bacteria_preferred_label), " resistant bacteria<br/>",
+                      str_sub(min(events$start_date), 1, 4), " - ",  str_sub(max(events$start_date), 1, 4))
+
+lf <- leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addFullscreenControl(position = "topright") %>%
+  addPolygons(data = admin, 
+              stroke = TRUE, color = "#46464a", weight = 1,
+              fill = TRUE, fillColor = ~pal(`AMR Events`), fillOpacity = 0.9,
+              label = ~paste0(name, ": ", `AMR Events`)) %>%
+  addLegend(data = admin, pal = pal, values = ~`AMR Events`, position = "bottomright") %>%
+  addCircleMarkers(data = study_locs,  radius = 3,
+                   lng = ~jitter(lon_study), lat = ~jitter(lat_study), 
+                   stroke = TRUE, color = "#210106", opacity = 1, weight = 1,
+                   fill = TRUE, fillColor = "#210106", fillOpacity = 0.5,
+                   label = ~study_location) %>%
+  addControl(caption)
+            
+lf
+htmlwidgets::saveWidget(lf, here("figures/map.html"))
